d4bd816440e6b05adecb19ddac84ad24471274f6
sabareshwaran <35525961+sabareshwaran@users.noreply.github.com>
2018-01-31 16:13:01 +0530

    Create largestnumber.c

---
+++ largestnumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n1,n2,n3;
+printf("enter a three variables:");
+scanf("%d%d%d",&n1,&n2,&n3);
+if(n1>n2 && n1>n2)
+printf("%d is a largest number",n1);
+if(n2>n1 && n2>n3)
+printf("%d is a largest number",n2);
+if(n3>n1 && n3>n2)
+printf("%d is alargest number",n3);
+return 0;
+}
